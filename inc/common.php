d103e785963e28efd263e49f3df0fbfb80d3ca05
mailmunch <mailmunch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-03 17:20:30 +0000

    sidebar widget enhancements version 1.3.8
    git-svn-id: https://plugins.svn.wordpress.org/mailmunch/tags/1.3.8@1018839 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/common.php
+++ inc/common.php
@@ -1,51 +1,73 @@
 <?php
-
-function getEmailPassword() {
-  $mailmunch_email = get_option("mailmunch_user_email");
-  $mailmunch_password = get_option("mailmunch_user_password");
-
-  if (empty($mailmunch_email)) {
-    $current_user = wp_get_current_user();
-    update_option("mailmunch_user_email", $current_user->user_email);
+class MailmunchHelpers {
+  function __construct() {
   }
 
-  if (empty($mailmunch_password)) {
-    update_option("mailmunch_user_password", uniqid());
+  function getEmailPassword() {
+    $mailmunch_email = get_option("mailmunch_user_email");
+    $mailmunch_password = get_option("mailmunch_user_password");
+
+    if (empty($mailmunch_email)) {
+      $current_user = wp_get_current_user();
+      update_option("mailmunch_user_email", $current_user->user_email);
+    }
+
+    if (empty($mailmunch_password)) {
+      update_option("mailmunch_user_password", uniqid());
+    }
+
+    $mailmunch_email = get_option("mailmunch_user_email");
+    $mailmunch_password = get_option("mailmunch_user_password");
+
+    return array('email' => $mailmunch_email, 'password' => $mailmunch_password);
   }
 
-  $mailmunch_email = get_option("mailmunch_user_email");
-  $mailmunch_password = get_option("mailmunch_user_password");
+  function getSite($sites, $site_id) {
+    foreach ($sites as $s) {
+      if ($s->id == intval($site_id)) {
+        $site = $s;
+        break;
+      }
+    }
 
-  return array('email' => $mailmunch_email, 'password' => $mailmunch_password);
-}
+    return $site;
+  }
 
-function getSite($sites, $site_id) {
-  foreach ($sites as $s) {
-    if ($s->id == intval($site_id)) {
-      $site = $s;
-      break;
+  function createAndGetSites($mm) {
+    $site_url = home_url();
+    $site_name = get_bloginfo();
+
+    if (!$mm->hasSite()) {
+      $mm->createSite($site_name, $site_url);
+    }
+    $request = $mm->sites();
+    if ($request['response']['code'] == 200){
+      $sites = $request['body'];
+
+      return json_decode($sites);
+    }
+    else {
+      return array();
     }
   }
 
-  return $site;
+  function createAndGetGuestSites($mm) {
+    // This is for GUEST users. Do NOT collect any user data.
+    $site_url = "";
+    $site_name = "WordPress";
+
+    if (!$mm->hasSite()) {
+      $mm->createSite($site_name, $site_url);
+    }
+    $request = $mm->sites();
+    if ($request['response']['code'] == 200){
+      $sites = $request['body'];
+
+      return json_decode($sites);
+    }
+    else {
+      return array();
+    }
+  }
 }
-
-function createAndGetSites($mm) {
-  $site_url = home_url();
-  $site_name = get_bloginfo();
-
-  if (!$mm->hasSite()) {
-    $mm->createSite($site_name, $site_url);
-  }
-  $request = $mm->sites();
-  if ($request->status_code == 200){
-    $sites = $request->body;
-
-    return json_decode($sites);
-  }
-  else {
-    return array();
-  }
-}
-
 ?>
